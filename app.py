7b717b6a5f167280293271fc5c6dd0f1b3a57b6e
Matthieu_Linux <matthieu.loic@icloud.com>
2023-08-25 13:46:10 +0400

    Added Login Functionality

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, redirect, session
 from cs50 import SQL
 from helpers import login_required
+from flask_session import Session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask("__name__")
 
 db = SQL("sqlite:///webApp.db")
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.after_request
 def after_request(response):
@@ -39,6 +45,30 @@ def searchResults():
 def login():
 
     if request.method == "POST":
+
+        # Get values posted by user
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        # If password or username is null
+        if not password or not username:
+            return redirect("/login")
+        
+        # Get user's record in database
+        user_record = db.execute("SELECT * FROM users WHERE username = ?", username)
+        # If returned record is null or greater than 2, redirect user to same page
+        if len(user_record) != 1:
+            print("USER DOES NOT EXIST")
+            return redirect("/login")
+
+        # If password hash in database does not match password posted, redirect user to login page
+        if not check_password_hash(user_record[0]["hash"], password):
+            print("INCORRECT PASSWORD")
+            return redirect("/login")
+        
+        # Assigns session to the user
+        session["user_id"] = user_record[0]["id"]
+
         return redirect("/")
     
     else:
